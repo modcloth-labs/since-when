48da855de8dd10fd9b6e6bee135e99672bf13615
Pat Bair <p.bair@modcloth.com>
2013-06-12 17:05:10 -0400

    genericized MissedRunCalculator

--- lib/since_when/missed_run_calculator.rb
+++ lib/since_when/missed_run_calculator.rb
@@ -3,33 +3,36 @@ require File.join(File.expand_path(File.dirname(__FILE__)), 'calculators/hour_ca
 
 module SinceWhen
   class MissedRunCalculator
+    VALID_INTERVALS = [:hour, :day]
 
-    def initialize(meta)
-      @meta = meta
+    def initialize(meta, interval)
+      @meta, @interval = meta, interval
     end
 
-    def find_by_day
-      Calculators::DayCalculator.new(meta.last_run).find
+    def find
+      raise ArgumentError, "invalid interval" unless valid?
+
+      calculator_for_interval.new(meta.last_run).find
     end
 
-    def find_by_hour
-      Calculators::HourCalculator.new(meta.last_run).find
-    end
-
-    def find_each_by_day
-      find_by_day.each do |day|
-        yield day
-      end
-    end
-
-    def find_each_by_hour
-      find_by_hour.each do |time|
+    def find_each
+      find.each do |time|
         yield time
       end
     end
 
     private
 
-    attr_reader :meta
+    def valid?
+      VALID_INTERVALS.include? interval
+    end
+
+    def calculator_for_interval
+      interval_name = interval.to_s.capitalize
+
+      Calculators.const_get("#{interval_name}Calculator")
+    end
+
+    attr_reader :meta, :interval
   end
 end
--- spec/lib/since_when/missed_run_calculator_spec.rb
+++ spec/lib/since_when/missed_run_calculator_spec.rb
@@ -4,19 +4,23 @@ describe SinceWhen::MissedRunCalculator do
   let(:time) { Time.utc(2013, 1, 1) }
   let(:meta) { double('meta', last_run: time) }
 
-  it "should provide each day since the last run" do
-    SinceWhen::Calculators::DayCalculator.any_instance.stub(:find).and_return([time])
 
-    described_class.new(meta).find_by_day do |t|
-      t.should == time
+  context "the given interval is valid" do
+    described_class::VALID_INTERVALS.each do |interval|
+      it "should provide each #{interval} since the last run" do
+        SinceWhen::Calculators.const_get("#{interval.to_s.capitalize}Calculator").
+          any_instance.stub(:find).and_return([time])
+
+        described_class.new(meta, interval).find do |t|
+          t.should == time
+        end
+      end
     end
   end
 
-  it "should provide each hour since the last run" do
-    SinceWhen::Calculators::HourCalculator.any_instance.stub(:find).and_return([time])
-
-    described_class.new(meta).find_by_hour do |t|
-      t.should == time
+  context "the given interval is not valid" do
+    it "should throw an error" do
+      expect { described_class.new(meta, :goat).find }.to raise_error
     end
   end
 end
